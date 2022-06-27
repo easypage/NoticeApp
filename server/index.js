0ef86cc9b1ca4b3e0095d2e3e924654fce757da9
EasyPage <74344815+easypage@users.noreply.github.com>
2022-06-27 11:20:19 +0900

    카카오 메세지 보내기 완성

--- server/index.js
+++ server/index.js
@@ -1,34 +1,37 @@
 // index.js
 const express = require("express"); // express 임포트
+const axios = require("axios");
+
 const app = express(); // app생성
 const port = 5000;
-
-app.get("/", function (req, res) {
-  const user = new User({
-    name: "kim",
-    age: 12,
-  });
-  user
-    .save()
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  res.send("hello world!!");
-});
-
-app.listen(port, () => console.log(`${port}포트입니다.`));
 
 // 몽구스 연결
 const mongoose = require("mongoose");
 const User = require("./Entity/userModel");
+const { json } = require("express");
+
+// 카카오 토큰
+const ACCESS_TOKEN = "7yF2_3ficSzfHTjH9Wy8hAwxfwjIHRY6X-rEDGBnCisNHwAAAYGi9Mlv";
+
+var dataString = `template_object={
+  "object_type": "text",
+  "text": "됬다!! 담배 피러간다.",
+  "link": {
+      "web_url": "https://developers.kakao.com",
+      "mobile_web_url": "https://developers.kakao.com"
+  },
+  "button_title": "바로 확인"
+}`;
+
+app.listen(port, () => {
+  console.log(`${port}포트입니다.`);
+});
+
 mongoose
   .connect(
     "mongodb+srv://minib:root@cluster0.y8vrqhs.mongodb.net/?retryWrites=true&w=majority",
     {
-      // useNewUrlPaser: true,
+      // useNewUrlPaser: true
       // useUnifiedTofology: true,
       // useCreateIndex: true,
       // useFindAndModify: false,
@@ -38,3 +41,38 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.get("/", async function (req, res) {
+  // const user = new User({
+  //   name: "lee",
+  //   age: 12,
+  // });
+  // await user
+  //   .save()
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
+
+  try {
+    await axios
+      .post(
+        "https://kapi.kakao.com/v2/api/talk/memo/default/send",
+        dataString,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded charset=utf-8",
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+          },
+        }
+      )
+      .catch((errer) => console.log(errer));
+  } catch (error) {
+    console.log(error);
+    console.log("에러발생");
+  }
+
+  res.send("hello world!!");
+});
