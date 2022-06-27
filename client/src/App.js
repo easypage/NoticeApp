9443b394067b4da53155c773a4391a1ccb3bea94
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-06-27 12:58:18 +0900

    캘린더한글적용
    그래

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Mycalendar from "./components/Mycalendar";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Mycalendar />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Mycalendar />
+    </div>
+  );
 }
 
 export default App;
